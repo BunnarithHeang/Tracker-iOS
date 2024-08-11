d045fae40fc209dd7a1cfe3360e9efe97a062c9f
Bunnarith Heang <heangbunnarith@gmail.com>
2024-08-11 11:37:03 +0700

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Heang Bunnarith on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
